c37704d22505f55a33a8f9390d52de8108ea6592
zobitobi89 <to2912@googlemai.com>
2012-04-04 13:00:34 +0200

    new files, please pull!

---
+++ src/sleimiPrototype/Controller.java
@@ -0,0 +1,14 @@
+package sleimiPrototype;
+
+public class Controller {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Sleimi");
+		System.out.println("ahjsldhla");
+	}
+
+}
