821c720a57ffe32b962b42f9d33a82b11143d605
THD <tufan.doenmez@googlemail.com>
2012-04-27 15:01:21 +0200

    PharmacyPanel edited

--- src/newOne/PharmacyPanel.java
+++ src/newOne/PharmacyPanel.java
@@ -3,17 +3,24 @@ package newOne;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+
+import newOne.PharmacyPanel;
+
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 public class PharmacyPanel extends JFrame {
 
 	private JPanel contentPane;
+	private PharmacyPanel pp;
 	//TODO test
 	private int state=0;
 	
@@ -31,34 +38,46 @@ public class PharmacyPanel extends JFrame {
 	 */
 	
 	public PharmacyPanel(PMgnt pm) {
+		setUndecorated(true);
 		setVisible(true);
+		this.pp = this;
+		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 150, 88);
+		setBounds(500, 100, 150, 88);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-
-		
-		JButton btnChemie = new JButton("Chemie");
+		ImageIcon iconchemie = new ImageIcon("src/images/iconchemie70x58.png");
+		JButton btnChemie = new JButton(iconchemie);
 		btnChemie.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				//setstate(1);
-				System.out.println("Chemie");
+				System.out.println("Chemo");
 			}
 		});
-		btnChemie.setPreferredSize(new Dimension(70, 29));
-		contentPane.add(btnChemie, BorderLayout.WEST);
+		btnChemie.setPreferredSize(new Dimension(67, 29));
 		
-		JButton btnHom = new JButton("Hom\u00F6");
+		ImageIcon iconhomo = new ImageIcon("src/images/iconhomo70x61.png");
+		JButton btnHom = new JButton(iconhomo);
 		btnHom.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				//setstate(2);
-				System.out.println("Hom�");
+				System.out.println("Homo");
 			}
 		});
-		btnHom.setPreferredSize(new Dimension(70, 29));
+		btnHom.setPreferredSize(new Dimension(67, 29));
+		contentPane.add(btnChemie, BorderLayout.WEST);
 		contentPane.add(btnHom, BorderLayout.EAST);
+				
+		contentPane.addMouseMotionListener(new MouseMotionListener() {
+			
+			@Override
+			public void mouseMoved(MouseEvent arg0) {
+			}
+			
+			@Override
+			public void mouseDragged(MouseEvent arg0) {
+				pp.setLocation((int)arg0.getLocationOnScreen().getX()-75, (int)arg0.getLocationOnScreen().getY()-44);
+			}
+		});
 	}
-
 }
