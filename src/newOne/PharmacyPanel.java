0e3146f195e93fe7fe66e781df2c7969a54516f6
Chris <Chris@134.155.23.125>
2012-04-29 18:04:47 +0200

    Several changes within pharmacy panel: 	-image paths refactored 	-images relocated 	-comments added 	-global refactoring

--- src/newOne/PharmacyPanel.java
+++ src/newOne/PharmacyPanel.java
@@ -19,8 +19,18 @@ import java.awt.event.MouseMotionListener;
 
 public class PharmacyPanel extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private JPanel contentPane;
-	private PharmacyPanel pp;
+	private PharmacyPanel pPanel;
+	private ImageIcon iconChemie;
+	private ImageIcon iconHomoe;
+	private JButton btnChemie;
+	private JButton btnHomoe;
+
 	//TODO test
 	private int state=0;
 	
@@ -38,36 +48,32 @@ public class PharmacyPanel extends JFrame {
 	 */
 	
 	public PharmacyPanel(PMgnt pm) {
-		setUndecorated(true);
+		this.pPanel = this;
+
 		setVisible(true);
-		this.pp = this;
+		setResizable(true);		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(210, 100, 180, 100);
 		
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(500, 100, 150, 88);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-		ImageIcon iconchemie = new ImageIcon("src/images/iconchemie70x58.png");
-		JButton btnChemie = new JButton(iconchemie);
-		btnChemie.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				System.out.println("Chemo");
-			}
-		});
-		btnChemie.setPreferredSize(new Dimension(67, 29));
-		
-		ImageIcon iconhomo = new ImageIcon("src/images/iconhomo70x61.png");
-		JButton btnHom = new JButton(iconhomo);
-		btnHom.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				System.out.println("Homo");
-			}
-		});
-		btnHom.setPreferredSize(new Dimension(67, 29));
-		contentPane.add(btnChemie, BorderLayout.WEST);
-		contentPane.add(btnHom, BorderLayout.EAST);
+		iconChemie = new ImageIcon("images/iconchemie70x58.png");
+		btnChemie = new JButton(iconChemie);
+		iconHomoe = new ImageIcon("images/iconhomo70x61.png");
+		btnHomoe = new JButton(iconHomoe);
 				
+		btnChemie.setPreferredSize(new Dimension(67, 29));
+		btnHomoe.setPreferredSize(new Dimension(67, 29));
+		contentPane.add(btnChemie, BorderLayout.WEST);
+		contentPane.add(btnHomoe, BorderLayout.EAST);
+		
+		/**
+		 * Contentpane mouselistener waiting for mouse dragged event. 
+		 * It extracts the current mouse position and relocates the 
+		 * pharmacy panel related to the changed mouse position and the size.
+		 */
 		contentPane.addMouseMotionListener(new MouseMotionListener() {
 			
 			@Override
@@ -76,8 +82,30 @@ public class PharmacyPanel extends JFrame {
 			
 			@Override
 			public void mouseDragged(MouseEvent arg0) {
-				pp.setLocation((int)arg0.getLocationOnScreen().getX()-75, (int)arg0.getLocationOnScreen().getY()-44);
+				pPanel.setLocation((int)arg0.getLocationOnScreen().getX()-75, (int)arg0.getLocationOnScreen().getY()-44);
 			}
 		});
+		
+		
+		/**
+		 * Workflow (Influence of homoeopatical pharmacy to particle system)
+		 * TODO - trigger "homoe" function contained in Events.class 
+		 */
+		btnHomoe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Homoe");
+			}
+		});
+		
+		/**
+		 * Workflow (Influence of chemical pharmacy to particle system)
+		 * TODO - trigger "chemie" function contained in Events.class
+		 */
+		btnChemie.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Chemie");
+			}
+		});
+		setContentPane(contentPane);
 	}
 }
